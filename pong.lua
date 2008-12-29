0bed8c4d9b500ecd20a01f54c8348a22d23e56ca
Adam Wiggins <adam@heroku.com>
2008-12-28 18:36:25 -0800

    Rect object

--- pong.lua
+++ pong.lua
@@ -1,12 +1,27 @@
-x = -0.5
-y = -0.5
+Rect = {x = 0, y = 0}
+
+function Rect:new(o)
+	o = o or {}
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
+
+function Rect:draw()
+	draw_rectangle(self.x, self.y, 50, 200)
+end
+
+function Rect:move()
+	if up_pressed()    then self.y = self.y + 0.01 end
+	if down_pressed()  then self.y = self.y - 0.01 end
+	if right_pressed() then self.x = self.x + 0.01 end
+	if left_pressed()  then self.x = self.x - 0.01 end
+end
+
+rect = Rect:new{x = -0.5, y = -0.5}
 
 function pulse()
-	draw_rectangle(x, y, 50, 200)
-
-	if up_pressed() then y = y + 0.01 end
-	if down_pressed() then y = y - 0.01 end
-	if right_pressed() then x = x + 0.01 end
-	if left_pressed() then x = x - 0.01 end
+	rect:move()
+	rect:draw()
 end
 
